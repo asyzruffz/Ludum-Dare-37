11c09cdb2a78e019533fb04625002996d5fed6f1
azfaralsukor <azfaralsukor@gmail.com>
2016-12-10 08:52:50 +0800

    Before Health Bar Bug

---
+++ Assets/Scripts/UpdateBossHealth.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateBossHealth : MonoBehaviour {
+
+	[Range(0,1)]
+	public float health;
+
+	private float healthBefore;
+	Rect rectHealth;
+
+	// Use this for initialization
+	void Start () {
+		rectHealth = GetComponent<RectTransform> ().rect;
+		healthBefore = rectHealth.width;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rectHealth.width = health * healthBefore;
+		GetComponent<RectTransform> ().offsetMin = rectHealth.min;
+		GetComponent<RectTransform> ().offsetMax = rectHealth.max;
+	}
+}
