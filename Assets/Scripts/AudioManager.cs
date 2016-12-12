e5058673841d59220f3bdebc08290bcc0552c5cc
azfaralsukor <azfaralsukor@gmail.com>
2016-12-12 21:54:41 +0800

    Audio script initial commit (tested)

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour{
+
+	public AudioClip[] audios;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void PlaySound (){
+		AudioSource audio = gameObject.AddComponent<AudioSource >();
+		audio.PlayOneShot (Randommizer());
+	}
+
+	AudioClip Randommizer (){
+		int size = audios.Length;
+		System.Random r = new System.Random();
+		int rIndex = r.Next(0, size);
+		return audios [rIndex];
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// test
+		if (true) {
+			PlaySound ();
+		}
+	}
+}
